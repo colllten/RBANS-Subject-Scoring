f270d4d07a159207cb2642fafb890b90ca4bd6df
Colten Glover <coltenglover@yahoo.com>
2023-07-27 21:03:22 -0400

    Added .gitignore and GUIs to take user through table selections

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,2 +1,37 @@
+import javax.swing.*;
+import java.io.File;
+
 public class Driver {
+    static File dataSheet;
+    public static void main(String[] args) {
+        do {
+            String filePath = JOptionPane.showInputDialog("Please enter the path to the data sheet\n" +
+                    "/Desktop/Folder1/Folder2/DataSheet.xlsx");
+            if (filePath.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Incorrect file path. Please try again.");
+            } else {
+                dataSheet = new File(filePath);
+            }
+        } while (!dataSheet.canRead());
+
+        String[] options = {"60 - 69", "50 - 59", "40 - 49", "20 - 39"};
+
+        //Ask for age range
+        int choice = JOptionPane.showOptionDialog(null, "What is your age range?", "Age Range",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, 0);
+        System.out.println(options[choice]);
+
+        //Ask for time
+        options = new String[]{"Post", "Mid", "Baseline"};
+        choice = JOptionPane.showOptionDialog(null, "What test stage?", "Test Stage",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, 0);
+        System.out.println(options[choice]);
+
+        //Ask for kind of test
+        options = new String[]{"Delayed Memory", "Attention", "Language", "Visuospatial/Constructional", "Immediate " +
+                "Memory"};
+        choice = JOptionPane.showOptionDialog(null, "What is the test type?", "Test Type",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, 0);
+        System.out.println(options[choice]);
+    }
 }
